a1e9dce4fb5bf1d604dca1f9cd6bc74281498c93
Fleur Van Buijten <fleurvanbuijten@live.be>
2023-04-21 20:21:28 +0200

    edit scripts for agent

--- Assets/Scripts/MLagent.cs
+++ Assets/Scripts/MLagent.cs
@@ -1,56 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Unity.MLAgents;
-using Unity.VisualScripting;
 using Unity.MLAgents.Actuators;
 using System;
-using static UnityEditor.Progress;
-using static UnityEngine.GraphicsBuffer;
 using Unity.MLAgents.Sensors;
-using UnityEditor.UIElements;
 
 public class MLagent : Agent
 {
     public float force = 20f;
     public float minVelocityLimit = 0.005f;
-    public GameObject Target;
-    public float negativeReward = 5f;
-    public float positiefReward = 1f;
+    public GameObject Target1;
+    public GameObject Target2;
+    public float negativeReward = 2f;
+    public float positiveReward = 1f;
     private Rigidbody rb;
-    private Rigidbody trb;
+    private Rigidbody trb1;
+    private Rigidbody trb2;
     private Vector3 spawnpoint = Vector3.zero;
-    //private bool died = false;
 
-    //float timeRunning = 0.0f;
-
-
-
-    //starting function
-
-    /* private void Start()
-     {
-         rb = this.GetComponent<Rigidbody>();
-         spawnpoint = transform.position;
-         rb.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
-     }*/
     public override void Initialize()
     {
         rb = this.GetComponent<Rigidbody>();
-        trb = Target.GetComponent<Rigidbody>();
+        trb1 = Target1.GetComponent<Rigidbody>();
+        trb2 = Target2.GetComponent<Rigidbody>();
         spawnpoint = transform.position;
         rb.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
     }
 
     public override void CollectObservations(VectorSensor sensor)
     {
-        // Target en Agent position
-        sensor.AddObservation(Target.transform.localPosition);
+        // Target1 en Agent position
+        sensor.AddObservation(Target1.transform.localPosition);
+        sensor.AddObservation(Target2.transform.localPosition);
         sensor.AddObservation(this.transform.localPosition);
 
         // Agent velocity
         sensor.AddObservation(rb.velocity.y);
-        sensor.AddObservation(trb.velocity.z);
+        sensor.AddObservation(trb1.velocity.z);
+        sensor.AddObservation(trb2.velocity.z);
     }
 
     public override void OnActionReceived(ActionBuffers actions)
@@ -59,11 +45,6 @@ public class MLagent : Agent
         {
             Thrust();
         }
-
-/*        if (Math.Abs(rb.velocity.y) < minVelocityLimit)
-        {
-            SetReward(0.5f);
-        }*/
     }
 
     public override void OnEpisodeBegin()
@@ -77,26 +58,6 @@ public class MLagent : Agent
         var discreteActionsOut = actionsOut.DiscreteActions;
         discreteActionsOut[0] = Input.GetKey(KeyCode.UpArrow) ? 1 : 0;
     }
-    /*    private void FixedUpdate()
-        {
-            if(Input.GetKey(KeyCode.UpArrow) == true)
-            {
-                Thrust();
-            }
-        }*/
-
-/*    private void Update()
-    {
-    
-       timeRunning += Time.deltaTime;
-        if (!died)
-        {
-            SetReward(timeRunning * positiefReward);
-            timeRunning = 0.0f; //Restart counting
-        }
-        
-        
-    }*/
 
     private void Thrust()
     {
@@ -110,14 +71,13 @@ public class MLagent : Agent
     {
         if (collision.gameObject.CompareTag("obstacle"))
         {
-            //died = true;
             AddReward(-negativeReward);
             EndEpisode();
         }
 
         if (collision.gameObject.CompareTag("points"))
         {
-            SetReward(positiefReward);
+            SetReward(positiveReward);
         }
     }
 }
